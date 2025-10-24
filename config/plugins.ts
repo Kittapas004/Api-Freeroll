aa4b339c3eecf60ae0226988051983ca8eaddfa8
Kittapas004 <6531503004@lamduan.mfu.ac.th>
2025-10-25 04:09:21 +0700

    fix forgotpassss

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,24 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST', 'smtp.sendgrid.net'),
+        port: parseInt(env('EMAIL_SMTP_PORT', '587')),
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: env('EMAIL_SMTP_USERNAME', 'apikey'),
+          pass: env('EMAIL_SMTP_PASSWORD'),
+        },
+        tls: {
+          rejectUnauthorized: false
+        }
+      },
+      settings: {
+        defaultFrom: env('EMAIL_FROM', 'patpatkittaphat@gmail.com'),
+        defaultReplyTo: env('EMAIL_FROM', 'patpatkittaphat@gmail.com'),
+        defaultFromName: env('EMAIL_FROM_NAME', 'TurmeRic'),
+      },
+    },
+  },
+});
