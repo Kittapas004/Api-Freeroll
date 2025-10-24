a9984687c61776ad6c08f969e21a2e85e0fec4f9
Kittapas004 <6531503004@lamduan.mfu.ac.th>
2025-10-25 06:32:29 +0700

    fixxyyy

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,12 @@
 export default ({ env }) => ({
   email: {
     config: {
-      provider: 'nodemailer',
+      provider: 'resend',
       providerOptions: {
-        service: 'gmail',
-        auth: {
-          user: env('EMAIL_FROM', 'patpatkittaphat@gmail.com'),
-          pass: env('GMAIL_APP_PASSWORD'), // Gmail App Password (NOT regular password)
-        },
+        apiKey: env('RESEND_API_KEY'),
       },
       settings: {
-        defaultFrom: env('EMAIL_FROM', 'patpatkittaphat@gmail.com'),
+        defaultFrom: env('EMAIL_FROM', 'onboarding@resend.dev'),
         defaultReplyTo: env('EMAIL_FROM', 'patpatkittaphat@gmail.com'),
         defaultFromName: env('EMAIL_FROM_NAME', 'TurmeRic'),
       },
